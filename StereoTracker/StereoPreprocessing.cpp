0638afce2c67ee1d345454805ffe4045f37af046
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2015-04-03 01:12:33 +0200

    Print Q projection matrix

--- StereoTracker/StereoPreprocessing.cpp
+++ StereoTracker/StereoPreprocessing.cpp
@@ -14,6 +14,8 @@ StereoPreprocessing::StereoPreprocessing(StereoCalibration* scp, Size imageSize,
 					scp->cameraMatrix[1], scp->distCoeffs[1],
 					this->imageSize, scp->R, scp->T, rp->R1, rp->R2, rp->P1, rp->P2, rp->Q,
 					CALIB_ZERO_DISPARITY, this->alpha, this->imageSize, &rp->validRoi[0], &rp->validRoi[1]);
+
+	cout << "Q = " << endl << rp->Q << endl << endl;
 	
 	initUndistortRectifyMap(scp->cameraMatrix[0], scp->distCoeffs[0], rp->R1, rp->P1, this->imageSize, CV_16SC2, rmap[0][0], rmap[0][1]);
 	initUndistortRectifyMap(scp->cameraMatrix[1], scp->distCoeffs[1], rp->R2, rp->P2, this->imageSize, CV_16SC2, rmap[1][0], rmap[1][1]);
