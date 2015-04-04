63cff6415220e48ab55be2bed1f1aad03d8f88db
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2015-04-04 16:13:40 +0200

    Extract KeyPointEx class to own file

--- StereoTracker/MotionTracking.cpp
+++ StereoTracker/MotionTracking.cpp
@@ -3,33 +3,6 @@
 using namespace cv;
 using namespace std;
 
-void KeyPointEx::convert(const std::vector<KeyPointEx*>& keypoints, std::vector<Point2f>& points2f,
-						const vector<int>& keypointIndexes)
-{
-	if( keypointIndexes.empty() )
-	{
-		points2f.resize( keypoints.size() );
-		for( size_t i = 0; i < keypoints.size(); i++ )
-			points2f[i] = keypoints[i]->pt;
-	}
-	else
-	{
-		points2f.resize( keypointIndexes.size() );
-		for( size_t i = 0; i < keypointIndexes.size(); i++ )
-		{
-			int idx = keypointIndexes[i];
-			if( idx >= 0 )
-				points2f[i] = keypoints[idx]->pt;
-			else
-			{
-				CV_Error( CV_StsBadArg, "keypointIndexes has element < 0. TODO: process this case" );
-				//points2f[i] = Point2f(-1, -1);
-			}
-		}
-	}
-}
-
-
 MotionTracking::MotionTracking(void)
 {
 	frame_number = 0;
--- StereoTracker/MotionTracking.h
+++ StereoTracker/MotionTracking.h
@@ -1,58 +1,9 @@
 #pragma once
 
-#include <math.h>
 #include "common.h"
-
+#include "KeyPointEx.h"
 
 using namespace cv;
-
-class KeyPointEx :
-	public cv::KeyPoint
-{
-public:
-	bool scheduledDelete;
-	int unusedFor;
-	Point2f lastMove;
-	KeyPointEx() : KeyPoint()
-	{
-		lastMove = Point2f(0,0);
-		scheduledDelete = false;
-		unusedFor = 0;
-	}
-	KeyPointEx(KeyPoint& kp)
-	{
-		this->pt = kp.pt;
-		this->size = kp.size;
-		this->angle = kp.angle;
-		this->response = kp.response;
-		this->octave = kp.octave;
-		this->class_id = kp.class_id;
-
-		lastMove = Point2f(0,0);
-		scheduledDelete = false;
-		unusedFor = 0;
-	}
-	bool sameAs(KeyPoint& kp)
-	{
-		return this->pt.x == kp.pt.x && this->pt.y == kp.pt.y;
-	}
-	bool sameAs(KeyPointEx& kp)
-	{
-		return this->pt.x == kp.pt.x && this->pt.y == kp.pt.y;
-	}
-	bool similiarAs(KeyPoint& kp)
-	{
-		return (std::abs(this->pt.x - kp.pt.x) + std::abs(this->pt.y - kp.pt.y)) < 10.0;
-	}
-	bool similiarAs(KeyPointEx& kp)
-	{
-		return (std::abs(this->pt.x - kp.pt.x) + std::abs(this->pt.y - kp.pt.y)) < 10.0;
-	}
-    static void convert(const vector<KeyPointEx*>& keypoints,
-                        CV_OUT vector<Point2f>& points2f,
-                        const vector<int>& keypointIndexes=vector<int>());
-};
-
 
 class MotionTracking
 {
@@ -65,5 +16,3 @@ public:
 	Ptr<FeatureDetector> fd;
 	StereoPair oldFrames;
 };
-
-
